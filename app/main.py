7921a368d19060278611821e35bb8dd9a5b3d57f
MahirSelek <63142764+MahirSelek@users.noreply.github.com>
2025-10-13 10:44:45 +0200

    update

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@
 # main.py v2
 
 import streamlit as st
+
+# Set page config at the very beginning
+st.set_page_config(layout="wide", page_title="Polo GGB Research Assistant", page_icon="favicon.svg")
 import platform
 import requests
 import time
@@ -947,7 +950,6 @@ def main():
         show_login_page()
         return
     
-    st.set_page_config(layout="wide", page_title="Polo GGB Research Assistant", page_icon="favicon.svg")
     current_dir = os.path.dirname(os.path.abspath(__file__))
     style_path = os.path.join(current_dir, "style.css")
     local_css(style_path)
